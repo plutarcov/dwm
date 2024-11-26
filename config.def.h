8188674f3f70be85cb0685a6f2a5c20429eb280c
Plutarco <joaofelipedasilva001@gmail.com>
2024-11-26 18:43:30 -0300

    config.def.h

--- config.def.h
+++ config.def.h
@@ -6,8 +6,9 @@ static const unsigned int gappx     = 15;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "CaskaydiaCoveNerdFontMono-Regular:size=12" };
-static const char dmenufont[]       = "CaskaydiaCoveNerdFontMono-Regular:size=12";
+static const char *fonts[]          = { "monospace:size=14:antialias=true:autohint
+ont-Regular:size=14:antialias=true:autohint=true" };
+static const char dmenufont[]       = "monospace:size=14";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
